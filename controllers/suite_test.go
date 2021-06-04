4725d208ad947ef660e9042833e136179a41a257
Colin Humphreys <hatofmonkeys@gmail.com>
2021-06-04 12:53:36 +0100

    Added a first integration test for PromiseReconciler
    Signed-off-by: Chris Hedley <chris@cghsystems.net>

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -17,11 +17,16 @@ limitations under the License.
 package controllers
 
 import (
+	"context"
+	"io/ioutil"
 	"path/filepath"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -32,6 +37,9 @@ import (
 
 	platformv1alpha1 "github.com/syntasso/synpl-platform/api/v1alpha1"
 	//+kubebuilder:scaffold:imports
+
+	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
@@ -39,6 +47,7 @@ import (
 
 var cfg *rest.Config
 var k8sClient client.Client
+var apiextensionClient *clientset.Clientset
 var testEnv *envtest.Environment
 
 func TestAPIs(t *testing.T) {
@@ -67,11 +76,57 @@ var _ = BeforeSuite(func() {
 
 	//+kubebuilder:scaffold:scheme
 
+	apiextensionClient = clientset.NewForConfigOrDie(cfg)
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
 	Expect(err).NotTo(HaveOccurred())
 	Expect(k8sClient).NotTo(BeNil())
 
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme: scheme.Scheme,
+	})
+	Expect(err).ToNot(HaveOccurred())
+
+	err = (&PromiseReconciler{
+		Manager:             k8sManager,
+		ApiextensionsClient: apiextensionClient,
+		Client:              k8sManager.GetClient(),
+		Scheme:              k8sManager.GetScheme(),
+		Log:                 ctrl.Log.WithName("controllers").WithName("PromiseReconciler"),
+	}).SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	go func() {
+		err = k8sManager.Start(ctrl.SetupSignalHandler())
+		Expect(err).ToNot(HaveOccurred())
+	}()
+
 }, 60)
+
+var _ = Context("Promise Reconciler", func() {
+	Describe("Creating Redis Promise CR", func() {
+		Describe("Dynamic Redis CRD is applied from Redis Promise CR", func() {
+			It("Creates an API for redis.redis.redis", func() {
+				promiseCR := &platformv1alpha1.Promise{}
+
+				yamlFile, err := ioutil.ReadFile("../config/samples/redis-promise.yaml")
+
+				Expect(err).ToNot(HaveOccurred())
+				err = yaml.Unmarshal(yamlFile, promiseCR)
+				Expect(err).ToNot(HaveOccurred())
+
+				promiseCR.Namespace = "default"
+				err = k8sClient.Create(context.Background(), promiseCR)
+				Expect(err).ToNot(HaveOccurred())
+
+				time.Sleep(4 * time.Second)
+
+				redisCRD, err := apiextensionClient.ApiextensionsV1().CustomResourceDefinitions().Get(context.Background(), "redis.redis.redis.opstreelabs.in", v1.GetOptions{})
+				Expect(err).ToNot(HaveOccurred())
+				Expect(redisCRD).ToNot(BeNil())
+			})
+		})
+	})
+})
 
 var _ = AfterSuite(func() {
 	By("tearing down the test environment")
--- go.mod
+++ go.mod
@@ -7,7 +7,6 @@ require (
 	github.com/minio/minio-go/v7 v7.0.10
 	github.com/onsi/ginkgo v1.14.1
 	github.com/onsi/gomega v1.10.2
-	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776
 	k8s.io/api v0.19.2
 	k8s.io/apiextensions-apiserver v0.19.2
 	k8s.io/apimachinery v0.21.0
