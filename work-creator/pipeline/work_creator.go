43a9fdb8583027f06a9db1e854880db0091a4655
Colin Humphreys <hatofmonkeys@gmail.com>
2021-07-07 16:45:05 +0100

    Created work creator
    Signed-off-by: Chris Hedley <chris@cghsystems.net>

---
+++ work-creator/pipeline/work_creator.go
@@ -0,0 +1,50 @@
+package pipeline
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+
+	platformv1alpha1 "github.com/syntasso/synpl-platform/api/v1alpha1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type WorkCreator struct {
+	K8sClient client.Client
+}
+
+func (w *WorkCreator) Execute(input_directory string) {
+
+	fmt.Println(input_directory)
+	//Read all files from input_directory
+	files, _ := ioutil.ReadDir(input_directory)
+	resources := []unstructured.Unstructured{}
+
+	for _, fileInfo := range files {
+		fileName := filepath.Join(input_directory, fileInfo.Name())
+		bytes, _ := ioutil.ReadFile(fileName)
+		unstructured := &unstructured.Unstructured{}
+		yaml.Unmarshal(bytes, unstructured)
+		resources = append(resources, *unstructured)
+	}
+
+	work := platformv1alpha1.Work{}
+	work.Name = "database"
+	work.Namespace = "default"
+
+	manifests := &work.Spec.Workload.Manifests
+	for _, resource := range resources {
+		manifest := platformv1alpha1.Manifest{
+			Unstructured: resource,
+		}
+		*manifests = append(*manifests, manifest)
+	}
+
+	err := w.K8sClient.Create(context.Background(), &work)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
---
+++ work-creator/test/integration/work_creator_test.go
@@ -0,0 +1,81 @@
+package integration_test
+
+import (
+	"context"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	platformv1alpha1 "github.com/syntasso/synpl-platform/api/v1alpha1"
+	"github.com/syntasso/synpl-platform/work-creator/pipeline"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+)
+
+var (
+	testEnv   *envtest.Environment
+	k8sClient client.Client
+)
+
+var _ = Describe("WorkCreator", func() {
+
+	BeforeSuite(func() {
+		//Env Test
+		By("bootstrapping test environment")
+		testEnv = &envtest.Environment{
+			CRDDirectoryPaths:     []string{filepath.Join("..", "..", "..", "config", "crd", "bases")},
+			ErrorIfCRDPathMissing: true,
+		}
+
+		cfg, err := testEnv.Start()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(cfg).NotTo(BeNil())
+
+		//Setup the k8s client
+		err = platformv1alpha1.AddToScheme(scheme.Scheme)
+		Expect(err).NotTo(HaveOccurred())
+		k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(k8sClient).NotTo(BeNil())
+
+		//don't run main
+		workCreator := pipeline.WorkCreator{
+			K8sClient: k8sClient,
+		}
+		inputDirectory, err := filepath.Abs("samples")
+		Expect(err).NotTo(HaveOccurred())
+		workCreator.Execute(inputDirectory)
+	}, 60)
+
+	Describe("Work in the API server", func() {
+		var work platformv1alpha1.Work
+		BeforeEach(func() {
+			work = getWork()
+		})
+
+		It("has a correctly configured Work resource", func() {
+			Expect(work.Name).To(Equal("database"))
+			Expect(work.Spec.Workload.Manifests).To(HaveLen(1))
+		})
+	})
+
+	AfterSuite(func() {
+		By("tearing down the test environment")
+		testEnv.Stop()
+	})
+})
+
+func getWork() platformv1alpha1.Work {
+	work := platformv1alpha1.Work{}
+	expectedName := types.NamespacedName{
+		//Name:      "${promise-identifier}-${namespace-anne-request}-${anne-request-name}",
+		Name:      "database",
+		Namespace: "default",
+	}
+	Expect(k8sClient).ToNot(BeNil())
+	err := k8sClient.Get(context.Background(), expectedName, &work)
+	Expect(err).ToNot(HaveOccurred())
+	return work
+}
