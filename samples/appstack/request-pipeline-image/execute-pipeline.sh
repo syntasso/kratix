5be1d7992ea64a4a3e15f5b43b977bb39e4a8403
Chris Hedley <chris@cghsystems.net>
2021-08-19 17:37:01 +0100

    Appstack Promise: Kpack, Knative, with the KappController

---
+++ samples/appstack/request-pipeline-image/execute-pipeline.sh
@@ -0,0 +1,14 @@
+#!/bin/sh 
+
+applicationName=$(yq eval '.spec.applicationName' /input/object.yaml) ; \
+gitRepo=$(yq eval '.spec.gitRepo' /input/object.yaml) ; \
+appImageTag=$(yq eval '.spec.appImageTag' /input/object.yaml) ; \
+builderImageTag=$(yq eval '.spec.builderImageTag' /input/object.yaml) ; \
+find /tmp/transfer -type f -exec sed -i \
+  -e "s/<tbr-gitRepo>/${gitRepo//\//\\/}/g" \
+  -e "s/<tbr-appImageTag>/${appImageTag//\//\\/}/g" \
+  -e "s/<tbr-builderImageTag>/${builderImageTag//\//\\/}/g" \
+  -e "s/<tbr-applicationName>/${applicationName//\//\\/}/g" \
+  {} \;  
+
+cp /tmp/transfer/* /output/
