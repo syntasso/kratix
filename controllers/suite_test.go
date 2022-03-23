d2c3e0b10195cd76715ef582cfd2c31d03f49e5f
Chris Hedley <chris@cghsystems.net>
2022-03-23 13:14:05 +0000

    Add test to cover CWR Work creation
    Signed-off-by: Colin Humphreys <hatofmonkeys@gmail.com>

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -38,7 +38,9 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
+	"github.com/syntasso/kratix/api/v1alpha1"
 	platformv1alpha1 "github.com/syntasso/kratix/api/v1alpha1"
+
 	//+kubebuilder:scaffold:imports
 
 	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
@@ -107,73 +109,82 @@ var _ = BeforeSuite(func() {
 }, 60)
 
 var _ = Context("Promise Reconciler", func() {
-	Describe("Creating Redis Promise CR", func() {
-		Describe("Dynamic Redis CRD is applied from Redis Promise CR", func() {
-			It("Creates an API for redis.redis.redis", func() {
+	Describe("Apply a Redis Promise", func() {
+		BeforeEach(func() {
+			yamlFile, err := ioutil.ReadFile("../config/samples/redis/redis-promise.yaml")
+			Expect(err).ToNot(HaveOccurred())
 
-				yamlFile, err := ioutil.ReadFile("../config/samples/redis/redis-promise.yaml")
-				Expect(err).ToNot(HaveOccurred())
+			promiseCR := &platformv1alpha1.Promise{}
+			err = yaml.Unmarshal(yamlFile, promiseCR)
+			promiseCR.Namespace = "default"
+			Expect(err).ToNot(HaveOccurred())
 
-				promiseCR := &platformv1alpha1.Promise{}
-				err = yaml.Unmarshal(yamlFile, promiseCR)
-				promiseCR.Namespace = "default"
-				Expect(err).ToNot(HaveOccurred())
+			//Works once, then fails as the promiseCR already exists. Consider building check here.
+			k8sClient.Create(context.Background(), promiseCR)
+		})
 
-				err = k8sClient.Create(context.Background(), promiseCR)
-				Expect(err).ToNot(HaveOccurred())
+		It("Creates an API for redis.redis.redis", func() {
+			var expectedAPI = "redis.redis.redis.opstreelabs.in"
+			var timeout = "30s"
+			var interval = "3s"
+			Eventually(func() string {
+				crd, _ := apiextensionClient.
+					ApiextensionsV1().
+					CustomResourceDefinitions().
+					Get(context.Background(), expectedAPI, metav1.GetOptions{})
 
-				var expectedAPI = "redis.redis.redis.opstreelabs.in"
-				var timeout = "30s"
-				var interval = "3s"
-				Eventually(func() string {
-					crd, _ := apiextensionClient.
-						ApiextensionsV1().
-						CustomResourceDefinitions().
-						Get(context.Background(), expectedAPI, metav1.GetOptions{})
+				// The returned CRD is missing the expected metadata,
+				// therefore we need to reach inside of the spec to get the
+				// underlying Redis crd defintion to allow us to assert correctly.
+				return crd.Spec.Names.Singular + "." + crd.Spec.Group
+			}, timeout, interval).Should(Equal(expectedAPI))
+		})
 
-					// The returned CRD is missing the expected metadata,
-					// therefore we need to reach inside of the spec to get the
-					// underlying Redis crd defintion to allow us to assert correctly.
-					return crd.Spec.Names.Singular + "." + crd.Spec.Group
-				}, timeout, interval).Should(Equal(expectedAPI))
-			})
+		It("Creates Redis Cluster Worker Resources", func() {
+			var timeout = "30s"
+			var interval = "3s"
+			Eventually(func() bool {
+				//Get the works object
+				expectedName := types.NamespacedName{
+					Name:      "redis-promise-default",
+					Namespace: "default",
+				}
+				err := k8sClient.Get(context.Background(), expectedName, &v1alpha1.Work{})
+				return err == nil
+			}, timeout, interval).Should(BeTrue())
 		})
 	})
 
-	//MISSING TEST: a Work is created with WCRs in it
+	Describe("Creating a Redis Custom Resource", func() {
+		It("Creates a valid pod spec for the transformation pipeline", func() {
+			yamlFile, err := ioutil.ReadFile("../config/samples/redis/redis-resource-request.yaml")
+			Expect(err).ToNot(HaveOccurred())
 
-	Describe("Creating a Redis CR", func() {
-		Describe("Creates a valid pod spec for the transformation pipeline", func() {
-			It("Creates a pod spec", func() {
-				yamlFile, err := ioutil.ReadFile("../config/samples/redis/redis-resource-request.yaml")
-				Expect(err).ToNot(HaveOccurred())
+			redisRequest := &unstructured.Unstructured{}
+			err = yaml.Unmarshal(yamlFile, redisRequest)
+			Expect(err).ToNot(HaveOccurred())
 
-				redisRequest := &unstructured.Unstructured{}
-				err = yaml.Unmarshal(yamlFile, redisRequest)
-				Expect(err).ToNot(HaveOccurred())
+			redisRequest.SetNamespace("default")
+			err = k8sClient.Create(context.Background(), redisRequest)
+			Expect(err).ToNot(HaveOccurred())
 
-				redisRequest.SetNamespace("default")
-				err = k8sClient.Create(context.Background(), redisRequest)
-				Expect(err).ToNot(HaveOccurred())
+			createdPod := v1.Pod{}
+			expectedName := types.NamespacedName{
+				//The name of the pod is generated dynamically by the Promise Controller. For testing purposes, we set a TEST_PROMISE_CONTROLLER_POD_IDENTIFIER_UUID via an environment variable in the Makefile to make the name deterministic
+				Name:      "request-pipeline-redis-promise-default-12345",
+				Namespace: "default",
+			}
 
-				createdPod := v1.Pod{}
-				expectedName := types.NamespacedName{
-					//The name of the pod is generated dynamically by the Promise Controller. For testing purposes, we set a TEST_PROMISE_CONTROLLER_POD_IDENTIFIER_UUID via an environment variable in the Makefile to make the name deterministic
-					Name:      "request-pipeline-redis-promise-default-12345",
-					Namespace: "default",
+			var timeout = "30s"
+			var interval = "3s"
+			Eventually(func() string {
+				err := k8sClient.Get(context.Background(), expectedName, &createdPod)
+				if err != nil {
+					fmt.Println(err.Error())
+					return ""
 				}
-
-				var timeout = "30s"
-				var interval = "3s"
-				Eventually(func() string {
-					err := k8sClient.Get(context.Background(), expectedName, &createdPod)
-					if err != nil {
-						fmt.Println(err.Error())
-						return ""
-					}
-					return createdPod.Spec.Containers[0].Name
-				}, timeout, interval).Should(Equal("writer"))
-			})
+				return createdPod.Spec.Containers[0].Name
+			}, timeout, interval).Should(Equal("writer"))
 		})
 	})
 })
