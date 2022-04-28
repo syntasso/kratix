787a25626beb9b4ec83aa0393a790382c837a447
Chris Hedley <chris@cghsystems.net>
2022-04-28 12:15:51 +0100

    Change order of Knative Serving Promise installation to after worker Flux install

--- samples/gcp-worker-cluster/request-pipeline-image/execute-pipeline.sh
+++ samples/gcp-worker-cluster/request-pipeline-image/execute-pipeline.sh
@@ -21,10 +21,6 @@ do
   echo "Worker Cluster is still creating in GCloud"
 done
 
-## Install Knative Promise
-kubectl apply -f https://raw.githubusercontent.com/syntasso/kratix/dev/samples/knative-serving/knative-serving-promise.yaml
-kubectl apply -f https://raw.githubusercontent.com/syntasso/kratix/dev/samples/knative-serving/knative-serving-resource-request.yaml
-
 ## Get GCloud Credentials so we can login to our GKE Cluster
 kubectl get secret -n crossplane-system $GCP_CREDS_SECRET_NAME --template={{.data.creds}} | base64 -d >> gcloud-creds.json
 gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file=gcloud-creds.json
@@ -36,3 +32,22 @@ find gitops-tk-resources.yaml -type f -exec sed -i -e "s/<tbr-minio-endpoint>/${
 ## Make GKE Cluster a Kratix Worker Cluster
 kubectl apply -f https://raw.githubusercontent.com/syntasso/kratix/dev/hack/worker/gitops-tk-install.yaml
 kubectl apply -f gitops-tk-resources.yaml 
+
+## wait until Kratix Namespace is created on the Worker cluster
+while true
+do
+  sleep 1
+  output="$(kubectl get ns 2>&1)"
+  echo $output
+  [[ $output =~ "kratix-worker-system" ]] && break
+  echo "Kratix is Still installing"
+done
+
+#Install Knative-Serving
+kubectl config unset current-context
+
+## Install Knative Promise
+kubectl apply -f https://raw.githubusercontent.com/syntasso/kratix/dev/samples/knative-serving/knative-serving-promise.yaml
+sleep 5 # Don't judge us, this was for a public demo! 
+## Ask for Knative Instance
+kubectl apply -f https://raw.githubusercontent.com/syntasso/kratix/dev/samples/knative-serving/knative-serving-resource-request.yaml
