1438b0bdf8a15b126bfaed6c5d6809a506c69389
Chris Hedley <chris@cghsystems.net>
2022-04-29 15:35:08 +0100

    Pass through the name of the resource request into Kratix Cluster and GKE cluster names

--- samples/spicy-dev-environment/request-pipeline-image/execute-pipeline.sh
+++ samples/spicy-dev-environment/request-pipeline-image/execute-pipeline.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 INPUT="/input/object.yaml"
+NAME=$(grep 'name:' $INPUT | tail -n1 | awk '{ print $2}')
 SERVICE_ACCOUNT=$(grep 'gcpServiceAccount:' $INPUT | tail -n1 | awk '{ print $2}')
 PROJECT=$(grep 'gcpProject:' $INPUT | tail -n1 | awk '{ print $2}')
 GCP_CREDS_SECRET_NAME=$(grep 'gcpSecretName:' $INPUT | tail -n1 | awk '{ print $2}')
@@ -9,13 +10,16 @@ kubectl crossplane install configuration salaboy/spicy-project-template-gcp:0.1.
 
 sleep 5
 
+find worker-cluster-resource.yaml -type f -exec sed -i -e "s/<tbr-name>/${NAME//\//\\/}/g" {} +; 
+find kratix-worker-cluster.yaml -type f -exec sed -i -e "s/<tbr-name>/${NAME//\//\\/}/g" {} +; 
+
 kubectl apply -f  kratix-worker-cluster.yaml
 kubectl apply -f  worker-cluster-resource.yaml
 
 while true
 do
   sleep 1
-  output="$(kubectl get spicyprojecttemplategcps.fmtok8s.salaboy.com my-spicy-project-gcp 2>&1)"
+  output="$(kubectl get spicyprojecttemplategcps.fmtok8s.salaboy.com $NAME 2>&1)"
   echo $output
   [[ $output =~ "True" ]] && break
   echo "Worker Cluster is still creating in GCloud"
@@ -25,7 +29,7 @@ done
 kubectl get secret -n crossplane-system $GCP_CREDS_SECRET_NAME --template={{.data.creds}} | base64 -d >> gcloud-creds.json
 gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file=gcloud-creds.json
 ## Sets KubeConfig to new GKE Cluster
-gcloud container clusters get-credentials cluster-my-spicy-project-gcp --region us-central1 --project $PROJECT
+gcloud container clusters get-credentials cluster-$NAME --region us-central1 --project $PROJECT
 
 find gitops-tk-resources.yaml -type f -exec sed -i -e "s/<tbr-minio-endpoint>/${MINIO_ENDPOINT//\//\\/}/g" {} +; 
 
