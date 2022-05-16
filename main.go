91a36f2aa7881e3f47f16f79b739fd4fdbc8769d
Chris Hedley <chris@cghsystems.net>
2022-05-16 15:46:33 +0100

    Create WorkPlacement object and controller

--- main.go
+++ main.go
@@ -120,6 +120,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Cluster")
 		os.Exit(1)
 	}
+	if err = (&controllers.WorkPlacementReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "WorkPlacement")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
