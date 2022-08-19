59599a9c2d5754e47d570b8c001571705e57765a
Abby Bangser <abby@syntasso.io>
2022-08-19 15:24:26 +0100

    Chore: Remove requirement for healthy Jenkins from CI
    Co-authored-by: Chris Hedley <chris@syntasso.io>
    Co-authored-by: Derik Evangelista <derik@syntasso.io>

--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -10,16 +10,14 @@ loops=0
 while ! kubectl --context kind-worker get pods | grep -q jenkins-example; do
     if (( loops > 45 )); then
         echo "timeout waiting for jenkins pod"
+        echo "=============== PIPELINE STATUS ==============="
+        kubectl --context kind-platform get pods
+        echo "=============== PIPELINE LOGS ==============="
+        kubectl --context kind-platform logs --selector kratix-promise-id=jenkins-promise-default --container xaas-request-pipeline-stage-1
+        echo "=============== WORKER EVENTS ==============="
+        kubectl --context kind-worker get events
         exit 1
     fi
     sleep 1
     loops=$(( loops + 1 ))
 done
-
-if ! kubectl --context kind-worker wait pod jenkins-example --for=condition=ready --timeout 180s; then
-    echo "================ LOGS ================"
-    kubectl --context kind-worker logs jenkins-example
-    echo "============== DESCRIBE =============="
-    kubectl --context kind-worker describe pod/jenkins-example
-    exit 1
-fi
