fd72e9c8e7fb05c3fa1b3c1bf6ec1d4e70d16451
Derik Evangelista <derik@syntasso.io>
2022-08-24 22:36:54 +0100

    fix: add missing field to scheduler

--- main.go
+++ main.go
@@ -112,6 +112,7 @@ func main() {
 		Client:       mgr.GetClient(),
 		Scheme:       mgr.GetScheme(),
 		BucketWriter: bucketWriter,
+		Scheduler:    &scheduler,
 		Log:          ctrl.Log.WithName("controllers").WithName("ClusterController"),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "Cluster")
