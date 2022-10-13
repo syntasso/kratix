c1b9570ad7bf363b24fbdd637e93139fda2079b2
Derik Evangelista <derik@syntasso.io>
2022-10-13 15:05:15 +0100

    Feat: add support for sepecifying cluster selector
    Co-authored-by: Jake Klein <jake@syntasso.io>

--- samples/knative-serving/request-pipeline-image/execute-pipeline.sh
+++ samples/knative-serving/request-pipeline-image/execute-pipeline.sh
@@ -1,3 +1,7 @@
-#!/bin/sh 
+#!/bin/sh
+export CLUSTER_SELECTOR="$(yq eval '.spec.type' /input/object.yaml)"
 
+if [ "${CLUSTER_SELECTOR}" != "null" ]; then
+  echo "environment: ${CLUSTER_SELECTOR}" > /metadata/cluster-selectors.yaml
+fi
 cp /tmp/transfer/* /output/
