f0c7977e31c429b089b32ffdbe25535f3de4b55b
Jake <jakelarsj@gmail.com>
2022-10-25 11:39:48 +0100

    chore: Update template
    - Rename asset file
    - Remove bash that will be copied from the workshop instead
    
    Co-authored-by: Jake Klein <jake@syntasso.io>

--- samples/.template/request-pipeline-image/execute-pipeline.sh
+++ samples/.template/request-pipeline-image/execute-pipeline.sh
@@ -1,12 +1,2 @@
 #!/bin/sh
-
 set -x
-
-# example of reading values from the provided resource request
-# note: Kratix will always load the resource request to /input/object.yaml
-export NAME=$(yq eval '.metadata.name' /input/object.yaml)
-
-# example of injecting values into the example asset
-# note: all kubernetes resources in /output will be applied to the worker cluster
-cat /tmp/transfer/example-asset.yaml | \
-  yq eval '.metadata.name = env(NAME) ' - > /output/example-asset.yaml
