71e92bfd67e22f70ea6ebbafd5780b588c3ee7e0
Winna Bridgewater <winnab@gmail.com>
2022-10-27 17:30:52 +0100

    Refactor: put Promise and dynamic controllers in separate files, with one test file
    [#183593258]

--- controllers/promise_controller.go
+++ controllers/promise_controller.go
@@ -19,13 +19,7 @@ package controllers
 import (
 	"context"
 	"encoding/json"
-	"k8s.io/apimachinery/pkg/types"
-	"os"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"time"
-
-	"fmt"
-	"strings"
 
 	"github.com/go-logr/logr"
 	"github.com/syntasso/kratix/api/v1alpha1"
@@ -36,14 +30,9 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/selection"
-	"k8s.io/apimachinery/pkg/util/uuid"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/yaml"
 )
 
 // PromiseReconciler reconciles a Promise object
@@ -54,37 +43,12 @@ type PromiseReconciler struct {
 	Manager             ctrl.Manager
 }
 
-type dynamicController struct {
-	client                 client.Client
-	gvk                    *schema.GroupVersionKind
-	scheme                 *runtime.Scheme
-	promiseIdentifier      string
-	promiseClusterSelector labels.Set
-	xaasRequestPipeline    []string
-	log                    logr.Logger
-}
-
 //+kubebuilder:rbac:groups=platform.kratix.io,resources=promises,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=platform.kratix.io,resources=promises/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=platform.kratix.io,resources=promises/finalizers,verbs=update
 
 //+kubebuilder:rbac:groups=apiextensions.k8s.io,resources=customresourcedefinitions,verbs=get;list;watch;create;update;patch;delete
 
-//+kubebuilder:rbac:groups="",resources=pods,verbs=create;list;watch
-//+kubebuilder:rbac:groups="",resources=serviceaccounts,verbs=create
-
-//+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterroles,verbs=create;escalate;bind
-//+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterrolebindings,verbs=create
-
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Promise object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.7.2/pkg/reconcile
 func (r *PromiseReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := r.Log.WithValues("promise", req.NamespacedName)
 
@@ -298,246 +262,4 @@ func (r *PromiseReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&v1alpha1.Promise{}).
 		Complete(r)
-}
-
-func (r *dynamicController) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	logger := r.log.WithValues(r.promiseIdentifier, req.NamespacedName)
-
-	resourceRequestIdentifier := fmt.Sprintf("%s-%s-%s", r.promiseIdentifier, req.Namespace, req.Name)
-
-	unstructuredCRD := &unstructured.Unstructured{}
-	unstructuredCRD.SetGroupVersionKind(*r.gvk)
-
-	err := r.client.Get(ctx, req.NamespacedName, unstructuredCRD)
-	if err != nil {
-		if errors.IsNotFound(err) {
-			return ctrl.Result{}, nil
-		}
-		logger.Error(err, "Failed getting Promise CRD")
-		return ctrl.Result{}, nil
-	}
-
-	finalizer := fmt.Sprintf("finalizers.%s.resource-request.kratix.io/work-cleanup", strings.ToLower(unstructuredCRD.GetKind()))
-
-	if !unstructuredCRD.GetDeletionTimestamp().IsZero() {
-		return r.deleteWork(ctx, unstructuredCRD, resourceRequestIdentifier, finalizer, r.log)
-	}
-
-	// Ensure the finalizer is present
-	if !controllerutil.ContainsFinalizer(unstructuredCRD, finalizer) {
-		//refactor shared func
-		controllerutil.AddFinalizer(unstructuredCRD, finalizer)
-		if err := r.client.Update(ctx, unstructuredCRD); err != nil {
-			//setup logger
-			//logger.Error(err, "failed to add finalizer")
-			return ctrl.Result{}, err
-		}
-		return ctrl.Result{}, nil
-	}
-
-	if r.pipelineHasExecuted(resourceRequestIdentifier) {
-		logger.Info("Cannot execute update on pre-existing pipeline for Promise resource request " + resourceRequestIdentifier)
-		return ctrl.Result{}, nil
-	}
-
-	workCreatorCommand := fmt.Sprintf("./work-creator -identifier %s -input-directory /work-creator-files", resourceRequestIdentifier)
-
-	configMap := v1.ConfigMap{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "cluster-selectors-" + r.promiseIdentifier,
-			Namespace: "default",
-		},
-		Data: map[string]string{
-			"selectors": labels.FormatLabels(r.promiseClusterSelector),
-		},
-	}
-	resourceRequestCommand := fmt.Sprintf("kubectl get %s.%s %s --namespace %s -oyaml > /output/object.yaml", strings.ToLower(r.gvk.Kind), r.gvk.Group, req.Name, req.Namespace)
-
-	pod := v1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "request-pipeline-" + r.promiseIdentifier + "-" + getShortUuid(),
-			Namespace: "default",
-			Labels: map[string]string{
-				"kratix-promise-id":                  r.promiseIdentifier,
-				"kratix-promise-resource-request-id": resourceRequestIdentifier,
-			},
-		},
-		Spec: v1.PodSpec{
-			RestartPolicy:      v1.RestartPolicyOnFailure,
-			ServiceAccountName: r.promiseIdentifier + "-sa",
-			Containers: []v1.Container{
-				{
-					Name: "writer",
-					//Image:   "syntasso/kratix-platform-work-creator:dev",
-					Image:   os.Getenv("WC_IMG"),
-					Command: []string{"sh", "-c", workCreatorCommand},
-					VolumeMounts: []v1.VolumeMount{
-						{
-							MountPath: "/work-creator-files/input",
-							Name:      "output",
-						},
-						{
-							MountPath: "/work-creator-files/metadata",
-							Name:      "metadata",
-						},
-						{
-							MountPath: "/work-creator-files/kratix-system",
-							Name:      "promise-cluster-selectors",
-						},
-					},
-				},
-			},
-			InitContainers: []v1.Container{
-				{
-					Name:    "reader",
-					Image:   "bitnami/kubectl:1.20.10",
-					Command: []string{"sh", "-c", resourceRequestCommand},
-					VolumeMounts: []v1.VolumeMount{
-						{
-							MountPath: "/output",
-							Name:      "input",
-						},
-					},
-				},
-				{
-					Name:  "xaas-request-pipeline-stage-1",
-					Image: r.xaasRequestPipeline[0],
-					//Command: Supplied by the image author via ENTRYPOINT/CMD
-					VolumeMounts: []v1.VolumeMount{
-						{
-							MountPath: "/input",
-							Name:      "input",
-						},
-						{
-							MountPath: "/output",
-							Name:      "output",
-						},
-						{
-							MountPath: "/metadata",
-							Name:      "metadata",
-						},
-					},
-				},
-			},
-			Volumes: []v1.Volume{
-				{
-					Name: "input",
-					VolumeSource: v1.VolumeSource{
-						EmptyDir: &v1.EmptyDirVolumeSource{},
-					},
-				},
-				{
-					Name: "output",
-					VolumeSource: v1.VolumeSource{
-						EmptyDir: &v1.EmptyDirVolumeSource{},
-					},
-				},
-				{
-					Name: "metadata",
-					VolumeSource: v1.VolumeSource{
-						EmptyDir: &v1.EmptyDirVolumeSource{},
-					},
-				},
-				{
-					Name: "promise-cluster-selectors",
-					VolumeSource: v1.VolumeSource{
-						ConfigMap: &v1.ConfigMapVolumeSource{
-							LocalObjectReference: v1.LocalObjectReference{
-								Name: "cluster-selectors-" + r.promiseIdentifier,
-							},
-							Items: []v1.KeyToPath{
-								{
-									Key:  "selectors",
-									Path: "promise-cluster-selectors",
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-
-	logger.Info("Creating Pipeline for Promise resource request: " + resourceRequestIdentifier + ". The pipeline will now execute...")
-	err = r.client.Create(ctx, &configMap)
-	if err != nil {
-		logger.Error(err, "Error creating config map")
-		y, _ := yaml.Marshal(&configMap)
-		logger.Error(err, string(y))
-	}
-	err = r.client.Create(ctx, &pod)
-	if err != nil {
-		logger.Error(err, "Error creating Pod")
-		y, _ := yaml.Marshal(&pod)
-		logger.Error(err, string(y))
-	}
-
-	return ctrl.Result{}, nil
-}
-
-func (r *dynamicController) pipelineHasExecuted(resourceRequestIdentifier string) bool {
-	isPromise, _ := labels.NewRequirement("kratix-promise-resource-request-id", selection.Equals, []string{resourceRequestIdentifier})
-	selector := labels.NewSelector().
-		Add(*isPromise)
-
-	listOps := &client.ListOptions{
-		Namespace:     "default",
-		LabelSelector: selector,
-	}
-
-	ol := &v1.PodList{}
-	err := r.client.List(context.Background(), ol, listOps)
-	if err != nil {
-		fmt.Println(err.Error())
-		return false
-	}
-	return len(ol.Items) > 0
-}
-
-func (r *dynamicController) deleteWork(ctx context.Context, resourceRequest *unstructured.Unstructured, workName string, finalizer string, logger logr.Logger) (ctrl.Result, error) {
-	if controllerutil.ContainsFinalizer(resourceRequest, finalizer) {
-		work := &v1alpha1.Work{}
-		err := r.client.Get(ctx, types.NamespacedName{
-			Namespace: "default",
-			Name:      workName,
-		}, work)
-		if err != nil {
-			if errors.IsNotFound(err) {
-				// only remove finalizer at this point because deletion success is guaranteed
-				controllerutil.RemoveFinalizer(resourceRequest, finalizer)
-				if err := r.client.Update(ctx, resourceRequest); err != nil {
-					return ctrl.Result{}, err
-				}
-			}
-
-			logger.Error(err, "Error locating Work %s, will try again in 5 seconds", "workName", workName)
-			return ctrl.Result{RequeueAfter: 5 * time.Second}, err
-		}
-
-		err = r.client.Delete(ctx, work)
-		if err != nil {
-			if errors.IsNotFound(err) {
-				// only remove finalizer at this point because deletion success is guaranteed
-				controllerutil.RemoveFinalizer(resourceRequest, finalizer)
-				if err := r.client.Update(ctx, resourceRequest); err != nil {
-					return ctrl.Result{}, err
-				}
-			}
-
-			logger.Error(err, "Error deleting Work %s, will try again in 5 seconds", "workName", workName)
-			return ctrl.Result{RequeueAfter: 5 * time.Second}, err
-		}
-	}
-
-	// requeue to ensure finalizer is removed
-	return ctrl.Result{RequeueAfter: 5 * time.Second}, nil
-}
-
-func getShortUuid() string {
-	envUuid, present := os.LookupEnv("TEST_PROMISE_CONTROLLER_POD_IDENTIFIER_UUID")
-	if present {
-		return envUuid
-	} else {
-		return string(uuid.NewUUID()[0:5])
-	}
 }
