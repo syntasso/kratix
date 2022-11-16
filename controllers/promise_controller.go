a09e2ccb9d2e35c1e666393dfe85076724042f16
Jake <jake@syntasso.io>
2022-11-16 16:37:41 +0000

    refactor: move shared code to utils.go
    - if someone can think of a better name for random shared code, please
      rename :D

--- controllers/promise_controller.go
+++ controllers/promise_controller.go
@@ -313,7 +313,7 @@ func (r *PromiseReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 		promiseClusterSelector: promise.Spec.ClusterSelector,
 		xaasRequestPipeline:    promise.Spec.XaasRequestPipeline,
 		log:                    r.Log,
-		uid:                    getShortUuid(),
+		uid:                    string(promise.GetUID())[0:5],
 		enabled:                &enabled,
 	}
 
@@ -425,39 +425,6 @@ func (r *PromiseReconciler) deleteDynamicControllerResources(ctx context.Context
 	return r.Client.Update(ctx, promise)
 }
 
-// pass in nil resourceLabels to delete all resources of the GVK
-func deleteAllResourcesWithKindMatchingLabel(ctx context.Context, kClient client.Client, gvk schema.GroupVersionKind, resourceLabels map[string]string, logger logr.Logger) (bool, error) {
-	resourceList := &unstructured.UnstructuredList{}
-	resourceList.SetGroupVersionKind(gvk)
-	listOptions := client.ListOptions{LabelSelector: labels.SelectorFromSet(resourceLabels)}
-	err := kClient.List(ctx, resourceList, &listOptions)
-	if err != nil {
-		return true, err
-	}
-
-	logger.Info("deleting resources", "kind", resourceList.GetKind(), "withLabels", resourceLabels, "resources", getResourceNames(resourceList.Items))
-
-	for _, resource := range resourceList.Items {
-		err = kClient.Delete(ctx, &resource)
-		if err != nil && !errors.IsNotFound(err) {
-			logger.Error(err, "Error deleting resource, will try again in 5 seconds", "name", resource.GetName(), "kind", resource.GetKind())
-			return true, err
-		}
-		logger.Info("successfully triggered deletion of resource", "name", resource.GetName(), "kind", resource.GetKind())
-	}
-
-	return len(resourceList.Items) != 0, nil
-}
-
-func getResourceNames(items []unstructured.Unstructured) []string {
-	var names []string
-	for _, item := range items {
-		names = append(names, item.GetName())
-	}
-
-	return names
-}
-
 func (r *PromiseReconciler) deleteResourceRequests(ctx context.Context, promise *v1alpha1.Promise, rrGVK schema.GroupVersionKind, logger logr.Logger) error {
 	// No need to pass labels since all resource requests are of Kind
 	resourcesRemaining, err := deleteAllResourcesWithKindMatchingLabel(ctx, r.Client, rrGVK, nil, logger)
@@ -544,37 +511,4 @@ func (r *PromiseReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&v1alpha1.Promise{}).
 		Complete(r)
-}
-
-// finalizers must be less than 64 characters
-func addFinalizers(ctx context.Context, client client.Client, resource client.Object, finalizers []string, logger logr.Logger) (ctrl.Result, error) {
-	logger.Info("Adding missing finalizers",
-		"expectedFinalizers", finalizers,
-		"existingFinalizers", resource.GetFinalizers(),
-	)
-	for _, finalizer := range finalizers {
-		controllerutil.AddFinalizer(resource, finalizer)
-	}
-	if err := client.Update(ctx, resource); err != nil {
-		return ctrl.Result{}, err
-	}
-	return ctrl.Result{RequeueAfter: defaultRequeue}, nil
-}
-
-func finalizersAreMissing(resource client.Object, finalizers []string) bool {
-	for _, finalizer := range finalizers {
-		if !controllerutil.ContainsFinalizer(resource, finalizer) {
-			return true
-		}
-	}
-	return false
-}
-
-func finalizersAreDeleted(resource client.Object, finalizers []string) bool {
-	for _, finalizer := range finalizers {
-		if controllerutil.ContainsFinalizer(resource, finalizer) {
-			return false
-		}
-	}
-	return true
 }
