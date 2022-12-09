463b2e1c17b1c29e1c3763c6712aabf9b208388b
Abby Bangser <abby@syntasso.io>
2022-12-09 16:22:31 +0000

    feat: Update scripts to allow use of gitea repository
    * Maintains minio as default
    * Reuses install-gitops across other scripts now
    
    [#183974268]

--- scripts/quick-start.sh
+++ scripts/quick-start.sh
@@ -4,6 +4,7 @@ set -eu
 
 ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
 source "${ROOT}/scripts/utils.sh"
+source "${ROOT}/scripts/install-gitops"
 
 KRATIX_DISTRIBUTION="${ROOT}/distribution/kratix.yaml"
 MINIO_INSTALL="${ROOT}/hack/platform/minio-install.yaml"
@@ -13,16 +14,18 @@ GITOPS_WORKER_RESOURCES="${ROOT}/hack/worker/gitops-tk-resources.yaml"
 
 RECREATE=false
 LOCAL_IMAGES=false
+GIT_REPO=false
 VERSION=${VERSION:-"$(cd $ROOT; git branch --show-current)"}
 DOCKER_BUILDKIT="${DOCKER_BUILDKIT:-1}"
 
-MINIO_TIMEOUT="180s"
+WAIT_TIMEOUT="180s"
 
 usage() {
-    echo -e "Usage: quick-start.sh [--help] [--recreate] [--local]"
+    echo -e "Usage: quick-start.sh [--help] [--recreate] [--local] [--git]"
     echo -e "\t--help, -h\t Prints this message"
     echo -e "\t--recreate, -r\t Deletes pre-existing KinD Clusters"
     echo -e "\t--local, -l\t Build and load Kratix images to KinD cache"
+    echo -e "\t--git, -g\t Use Gitea as local repository in place of default local MinIO"
     exit "${1:-0}"
 }
 
@@ -33,17 +36,19 @@ load_options() {
         '--help')     set -- "$@" '-h'   ;;
         '--recreate') set -- "$@" '-r'   ;;
         '--local')    set -- "$@" '-l'   ;;
+        '--git')      set -- "$@" '-g'   ;;
         *)            set -- "$@" "$arg" ;;
       esac
     done
 
     OPTIND=1
-    while getopts "hrl" opt
+    while getopts "hrlg" opt
     do
       case "$opt" in
         'r') RECREATE=true ;;
         'h') usage ;;
         'l') LOCAL_IMAGES=true ;;
+        'g') GIT_REPO=true;;
         *) usage 1 ;;
       esac
     done
@@ -111,13 +116,6 @@ verify_prerequisites() {
     fi
 }
 
-patch_kind_networking() {
-    PLATFORM_CLUSTER_IP=`docker inspect platform-control-plane | grep '"IPAddress": "172' | awk '{print $2}' | awk -F '"' '{print $2}'`
-    sed -i'' -e "s/172.18.0.2/$PLATFORM_CLUSTER_IP/g" ${ROOT}/hack/worker/gitops-tk-resources.yaml
-    rm -f ${ROOT}/hack/worker/gitops-tk-resources.yaml-e
-}
-
-
 _build_kratix_image() {
     docker build --tag syntasso/kratix-platform:${VERSION} --quiet --file ${ROOT}/Dockerfile ${ROOT} &&
     kind load docker-image syntasso/kratix-platform:${VERSION} --name platform
@@ -144,24 +142,50 @@ build_and_load_local_images() {
     fi
 }
 
+patch_distribution() {
+    sed -i'' -e "s/--repository-type=s3/--repository-type=git/g" ${KRATIX_DISTRIBUTION}
+    rm -f ${KRATIX_DISTRIBUTION}-e
+}
+
 setup_platform_cluster() {
-    kubectl --context kind-platform apply --filename "${MINIO_INSTALL}"
+    if ${GIT_REPO}; then
+        kubectl --context kind-platform apply --filename "${ROOT}/hack/platform/gitea-install.yaml"
+        patch_distribution
+    else
+        kubectl --context kind-platform apply --filename "${MINIO_INSTALL}"
+    fi
     kubectl --context kind-platform apply --filename "${KRATIX_DISTRIBUTION}"
 }
 
 setup_worker_cluster() {
     kubectl --context kind-platform apply --filename "${PLATFORM_WORKER}"
     kubectl --context kind-worker apply  --filename "${GITOPS_WORKER_INSTALL}"
-    kubectl --context kind-worker apply --filename "${GITOPS_WORKER_RESOURCES}"
+    if ${GIT_REPO}; then
+        kubectl --context kind-worker apply --filename "${ROOT}/hack/worker/gitops-tk-resources-git.yaml"
+    else
+        kubectl --context kind-worker apply --filename "${GITOPS_WORKER_RESOURCES}"
+    fi
+}
+
+wait_for_gitea() {
+    kubectl wait pod --context kind-platform -n gitea --selector app=gitea --for=condition=ready ${opts}
 }
 
 wait_for_minio() {
+    kubectl wait pod --context kind-platform -n kratix-platform-system --selector run=minio --for=condition=ready ${opts}
+}
+
+wait_for_local_repository() {
     local timeout_flag="${1:-""}"
     opts=""
     if [ -z "${timeout_flag}" ]; then
-        opts="--timeout=${MINIO_TIMEOUT}"
+        opts="--timeout=${WAIT_TIMEOUT}"
     fi
-    kubectl wait pod --context kind-platform -n kratix-platform-system --selector run=minio --for=condition=ready ${opts}
+    if ${GIT_REPO}; then
+        wait_for_gitea
+    else
+        wait_for_minio
+    fi
 }
 
 wait_for_namespace() {
@@ -198,21 +222,19 @@ install_kratix() {
         exit 1
     fi
 
-    patch_kind_networking
-
     log -n "Creating worker cluster..."
     if ! run kind create cluster --name worker --image kindest/node:v1.24.0; then
         error "Could not create worker cluster"
         exit 1
     fi
 
-    log -n "Waiting for MinIO to be running..."
-    if ! SUPRESS_OUTPUT=true run wait_for_minio; then
-        log "\n\nIt's taking longer than usual for MinIO to start."
+    log -n "Waiting for local repository to be running..."
+    if ! SUPRESS_OUTPUT=true run wait_for_local_repository; then
+        log "\n\nIt's taking longer than usual for the local repository to start."
         log "You can check the platform pods to ensure there are no errors."
-        log "This script will continue to wait for MinIO to come up. You can kill it with $(info "CTRL+C.")"
-        log -n "\nWaiting for MinIO to be running... "
-        run wait_for_minio --no-timeout
+        log "This script will continue to wait for the local repository to come up. You can kill it with $(info "CTRL+C.")"
+        log -n "\nWaiting for the local repository to be running... "
+        run wait_for_local_repository --no-timeout
     fi
 
     log -n "Setting up worker cluster..."
@@ -226,11 +248,10 @@ install_kratix() {
         log "\n\nIt's taking longer than usual for the system to reconcile."
         log "You can check the pods on the platform and worker clusters for debugging information."
         log "This script will continue to wait. You can kill it with $(info "CTRL+C.")"
-        log -n "\nWaiting for MinIO to be running... "
+        log -n "\nWaiting for local repository to be running... "
         run wait_for_namespace --no-timeout
     fi
 
-    git checkout hack/
     kubectl config use-context kind-platform >/dev/null
     success "Kratix installation is complete!"
 }
