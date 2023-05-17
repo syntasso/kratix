2e548900217705712b76672062f39622fd04ecc7
Jake <jake@syntasso.io>
2023-05-17 16:57:50 +0100

    chore: remove deadcode

--- lib/writers/statestore_writer.go
+++ lib/writers/statestore_writer.go
@@ -4,8 +4,3 @@ type StateStoreWriter interface {
 	WriteObject(path string, objectName string, toWrite []byte) error
 	RemoveObject(bucketName string, objectName string) error
 }
-
-const (
-	S3  string = "s3"
-	Git string = "git"
-)
