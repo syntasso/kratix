e6558c7482cb4a80adf6654fb91c55ea35c08b23
Abby Bangser <abby@syntasso.io>
2023-08-30 15:24:15 +0100

    fix: remove log noise from kratix start up reconciliation

--- lib/writers/s3.go
+++ lib/writers/s3.go
@@ -66,6 +66,7 @@ func (b *S3Writer) WriteDirWithObjects(deleteExistingContentsInDir bool, dir str
 		var err error
 		objectsToDeleteMap, err = b.getObjectsInDir(ctx, dir, logger)
 		if err != nil {
+			logger.Error(err, "Error getting objects to delete")
 			return err
 		}
 	}
@@ -73,8 +74,9 @@ func (b *S3Writer) WriteDirWithObjects(deleteExistingContentsInDir bool, dir str
 	exists, errBucketExists := b.RepoClient.BucketExists(ctx, b.BucketName)
 	if errBucketExists != nil {
 		logger.Error(errBucketExists, "Could not verify bucket existence with provider")
+		return errBucketExists
 	} else if !exists {
-		logger.Info("Bucket provided does not exist (or the provided keys don't have permissions)")
+		return fmt.Errorf("Bucket provided does not exist (or the provided keys don't have permissions)")
 	}
 
 	for _, item := range toWrite {
