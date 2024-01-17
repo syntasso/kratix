4ec9df17a7f949986f75de65e9a4b3acbca0a929
Jake Klein <jake@syntasso.io>
2024-01-17 11:11:06 +0000

    chore: run system tests in parallel (#70)
    Co-authored-by: Sapphire Mason-Brown <sapphire@syntasso.io>

--- test/system/assets/bash-promise/dependencies.sh
+++ test/system/assets/bash-promise/dependencies.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env sh
-set -euo pipefail
-set +x
+set -eux
 
+unique_id=$(yq '.metadata.name' /kratix/input/object.yaml)
+
+imperative_platform_namespace=${unique_id}-platform-imperative
+declarative_worker_namespace=${unique_id}-worker-declarative-${VERSION}
+declarative_platform_namespace=${unique_id}-platform-declarative
 
 if [ "${KRATIX_WORKFLOW_ACTION}" = "configure" ]; then
-  kubectl create namespace bash-workflow-namespace-${VERSION} --dry-run=client -oyaml > /kratix/output/namespace.yaml
-  yq -i '.metadata.labels.modifydepsinpipeline = "yup"' /kratix/output/static/dependencies.yaml
+	kubectl create namespace ${declarative_worker_namespace} --dry-run=client -oyaml > /kratix/output/namespace.yaml
+	yq -i '.metadata.labels.modifydepsinpipeline = "yup"' /kratix/output/static/dependencies.yaml
 
-  mkdir -p /kratix/output/platform/
-  kubectl create namespace promise-workflow-namespace --dry-run=client -oyaml > /kratix/output/platform/namespace.yaml
-  cat <<EOF > /kratix/metadata/destination-selectors.yaml
+	mkdir -p /kratix/output/platform/
+	kubectl create namespace ${declarative_platform_namespace} --dry-run=client -oyaml > /kratix/output/platform/namespace.yaml
+	cat <<EOF > /kratix/metadata/destination-selectors.yaml
   - directory: platform
     matchLabels:
       environment: platform
@@ -17,10 +21,10 @@ if [ "${KRATIX_WORKFLOW_ACTION}" = "configure" ]; then
       extra: label
 EOF
 
-  kubectl get namespace $(yq '.metadata.name' /kratix/input/object.yaml)-workflow-imperative-namespace || kubectl create namespace $(yq '.metadata.name' /kratix/input/object.yaml)-workflow-imperative-namespace
-  exit 0
+	kubectl get namespace ${imperative_platform_namespace} || kubectl create namespace ${imperative_platform_namespace}
+	exit 0
 fi
 
-cat /kratix/input/object.yaml
-kubectl delete namespace $(yq '.metadata.name' /kratix/input/object.yaml)-workflow-imperative-namespace
+kubectl delete namespace ${imperative_platform_namespace}
+
 
