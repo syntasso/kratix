c53edbbe1550105c4aeb8877afe94c6fd529d6a6
Jake <jake@syntasso.io>
2024-03-20 09:31:53 +0000

    fix failing test

--- lib/pipeline/delete_test.go
+++ lib/pipeline/delete_test.go
@@ -40,7 +40,8 @@ var _ = Describe("Delete Pipeline", func() {
 				Name:      "custom-namespace-promise-pipeline",
 				Namespace: "kratix-platform-system",
 				Labels: map[string]string{
-					"kratix-promise-id": "custom-namespace",
+					"kratix-promise-id":      "custom-namespace",
+					"kratix.io/promise-name": "custom-namespace",
 				},
 			}
 		)
@@ -128,6 +129,8 @@ var _ = Describe("Delete Pipeline", func() {
 					"kratix-workflow-type":            Equal("promise"),
 					"kratix-workflow-action":          Equal("delete"),
 					"kratix-promise-id":               Equal("custom-namespace"),
+					"kratix.io/promise-name":          Equal("custom-namespace"),
+					"kratix-workflow-pipeline-name":   Equal("promise-delete"),
 				})
 
 				Expect(job).To(MatchFields(IgnoreExtras, Fields{
@@ -227,7 +230,8 @@ var _ = Describe("Delete Pipeline", func() {
 				Name:      "custom-namespace-resource-pipeline",
 				Namespace: "default",
 				Labels: map[string]string{
-					"kratix-promise-id": "custom-namespace",
+					"kratix-promise-id":      "custom-namespace",
+					"kratix.io/promise-name": "custom-namespace",
 				},
 			}
 		)
@@ -317,7 +321,9 @@ var _ = Describe("Delete Pipeline", func() {
 					"kratix-workflow-type":               Equal("resource"),
 					"kratix-workflow-action":             Equal("delete"),
 					"kratix-promise-id":                  Equal("custom-namespace"),
+					"kratix.io/promise-name":             Equal("custom-namespace"),
 					"kratix-promise-resource-request-id": Equal("example-custom-namespace"),
+					"kratix-workflow-pipeline-name":      Equal("instance-delete"),
 				})
 
 				Expect(job).To(MatchFields(IgnoreExtras, Fields{
