9688d63dd767cc40e11126b3c2ca566e16042a66
Derik Evangelista <derik@syntasso.io>
2024-03-28 11:08:04 +0000

    feat: use pipeline name at write to state store
    when a promise contains multiple workflows, we need a mechanism to stop
    workflows from overwriting or removing files from each other; we can use
    the pipeline name to create a unique directory at the state store

--- controllers/workplacement_controller.go
+++ controllers/workplacement_controller.go
@@ -152,7 +152,7 @@ func (r *WorkPlacementReconciler) removeWorkFromRepository(writer writers.StateS
 func getDir(workPlacement v1alpha1.WorkPlacement) string {
 	if workPlacement.Spec.ResourceName == "" {
 		//dependencies/<promise-name>/<dir-sha>/
-		return filepath.Join(dependenciesDir, workPlacement.Spec.PromiseName, shortID(workPlacement.Spec.ID))
+		return filepath.Join(dependenciesDir, workPlacement.Spec.PromiseName, workPlacement.PipelineName(), shortID(workPlacement.Spec.ID))
 	} else {
 		//resources/<rr-namespace>/<promise-name>/<rr-name>/<dir-sha>/
 		return filepath.Join(resourcesDir, workPlacement.GetNamespace(), workPlacement.Spec.PromiseName, workPlacement.Spec.ResourceName, shortID(workPlacement.Spec.ID))
