cc9993521ad30efd8689faa5c366b9e5cc00a4d6
Derik Evangelista <derik@syntasso.io>
2025-01-28 17:49:54 +0000

    fix: promise release system test [skip ci]
    Co-authored-by: Chunyi Lyu <chunyi@syntasso.io>

--- test/system/promise_release_test.go
+++ test/system/promise_release_test.go
@@ -1,46 +1,51 @@
 package system_test
 
 import (
+	"time"
+
 	. "github.com/onsi/ginkgo/v2"
-	"github.com/onsi/ginkgo/v2/types"
 	. "github.com/onsi/gomega"
 )
 
-var _ = FDescribe("PromiseRelease", func() {
-	BeforeEach(func() {
-		platform.kubectl("apply", "-f", "assets/promise-release/promise-release.yaml")
-		platform.kubectl("apply", "-f", "assets/promise-release/deployment.yaml")
-	})
-
-	AfterEach(func() {
-		if CurrentSpecReport().State.Is(types.SpecStatePassed) {
-			platform.kubectl("delete", "-f", "assets/promise-release/deployment.yaml")
-		}
-	})
-
-	When("fetching Promise from plain http endpoint", func() {
-		It("can create and delete the Promise", func() {
-			platform.eventuallyKubectl("get", "promiserelease", "insecure")
-			platform.eventuallyKubectl("get", "promise", "insecurepro")
-
-			platform.eventuallyKubectlDelete("promiserelease", "insecure")
-			Eventually(func(g Gomega) {
-				g.Expect(platform.kubectl("get", "promise")).ShouldNot(ContainSubstring("insecurepro"))
-				g.Expect(platform.kubectl("get", "promiserelease")).ShouldNot(ContainSubstring("insecure"))
-			}).Should(Succeed())
+var _ = Describe("PromiseRelease", func() {
+	Describe("sourceRef.type == http", func() {
+		BeforeEach(func() {
+			platform.kubectl("apply", "-f", "assets/promise-release/deployment.yaml")
+			platform.kubectl("wait", "-n", "kratix-platform-system", "deployments", "kratix-promise-release-test-hoster", "--for=condition=Available")
 		})
-	})
 
-	When("fetching Promise from http endpoint with authorization", func() {
-		It("can fetch and apply the Promise", func() {
-			platform.eventuallyKubectl("get", "promiserelease", "secure")
-			platform.eventuallyKubectl("get", "promise", "securepro")
+		AfterEach(func() {
+			platform.kubectl("delete", "-f", "assets/promise-release/deployment.yaml")
+		})
 
-			platform.eventuallyKubectlDelete("promiserelease", "secure")
-			Eventually(func(g Gomega) {
-				g.Expect(platform.kubectl("get", "promise")).ShouldNot(ContainSubstring("securepro"))
-				g.Expect(platform.kubectl("get", "promiserelease")).ShouldNot(ContainSubstring("secure"))
-			}).Should(Succeed())
+		It("can fetch and manage the promise from the specified url", func() {
+			Eventually(func() string {
+				return platform.kubectl("apply", "-f", "assets/promise-release/promise-release.yaml")
+			}, 30*time.Second, 1*time.Second).Should(ContainSubstring("promiserelease.platform.kratix.io/secure created"))
+
+			By("pulling the Promise from plain http endpoints", func() {
+				platform.eventuallyKubectl("get", "promiserelease", "insecure")
+				platform.eventuallyKubectl("get", "promise", "insecurepro")
+			})
+
+			By("pulling the Promise from http endpoints with authorization", func() {
+				platform.eventuallyKubectl("get", "promiserelease", "secure")
+				platform.eventuallyKubectl("get", "promise", "securepro")
+			})
+
+			By("deleting all resources created by the PromiseRelease on deletion", func() {
+				platform.eventuallyKubectlDelete("promiserelease", "insecure")
+				Eventually(func(g Gomega) {
+					g.Expect(platform.kubectl("get", "promise")).ShouldNot(ContainSubstring("insecurepro"))
+					g.Expect(platform.kubectl("get", "promiserelease")).ShouldNot(ContainSubstring("insecure"))
+				}).Should(Succeed())
+
+				platform.eventuallyKubectlDelete("promiserelease", "secure")
+				Eventually(func(g Gomega) {
+					g.Expect(platform.kubectl("get", "promise")).ShouldNot(ContainSubstring("securepro"))
+					g.Expect(platform.kubectl("get", "promiserelease")).ShouldNot(ContainSubstring("secure"))
+				}).Should(Succeed())
+			})
 		})
 	})
 })
