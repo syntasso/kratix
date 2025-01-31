2da5e2b15107585350765ed98b7dbd7508aa9f7a
Derik Evangelista <derik@syntasso.io>
2025-01-31 17:20:13 +0000

    fix: make the linter happy

--- main.go
+++ main.go
@@ -73,12 +73,12 @@ type Workflows struct {
 	DefaultContainerSecurityContext corev1.SecurityContext `json:"defaultContainerSecurityContext"`
 }
 
-func main() {
-	var metricsAddr string
-	var probeAddr string
-	var pprofAddr string
-	var enableLeaderElection bool
+var metricsAddr string
+var probeAddr string
+var pprofAddr string
+var enableLeaderElection bool
 
+func main() {
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.StringVar(&pprofAddr, "pprof-bind-address", ":8082", "The address the pprof endpoint binds to.")
