410b43b9b5072489824104411b98c2265fd7fa71
Chunyi Lyu <chunyineko@gmail.com>
2025-05-15 12:45:22 +0100

    chore: clean up generated comments in bucket webhook

--- internal/webhook/v1alpha1/bucketstatestore_webhook.go
+++ internal/webhook/v1alpha1/bucketstatestore_webhook.go
@@ -22,7 +22,6 @@ import (
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -30,10 +29,8 @@ import (
 	v1alpha1 "github.com/syntasso/kratix/api/v1alpha1"
 )
 
-// log is for logging in this package.
 var bucketstatestorelog = logf.Log.WithName("bucketstatestore-resource")
 
-// SetupBucketStateStoreWebhookWithManager registers the webhook for BucketStateStore in the manager.
 func SetupBucketStateStoreWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr).For(&v1alpha1.BucketStateStore{}).
 		WithValidator(&BucketStateStoreCustomValidator{}).
@@ -42,18 +39,13 @@ func SetupBucketStateStoreWebhookWithManager(mgr ctrl.Manager) error {
 
 // +kubebuilder:webhook:path=/validate-platform-kratix-io-v1alpha1-bucketstatestore,mutating=false,failurePolicy=fail,sideEffects=None,groups=platform.kratix.io,resources=bucketstatestores,verbs=create;update,versions=v1alpha1,name=vbucketstatestore-v1alpha1.kb.io,admissionReviewVersions=v1
 
-// BucketStateStoreCustomValidator struct is responsible for validating the BucketStateStore resource
-// when it is created, updated, or deleted.
-//
 // NOTE: The +kubebuilder:object:generate=false marker prevents controller-gen from generating DeepCopy methods,
 // as this struct is used only for temporary operations and does not need to be deeply copied.
 type BucketStateStoreCustomValidator struct {
-	Client client.Client
 }
 
 var _ webhook.CustomValidator = &BucketStateStoreCustomValidator{}
 
-// ValidateCreate implements webhook.CustomValidator so a webhook will be registered for the type BucketStateStore.
 func (v *BucketStateStoreCustomValidator) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
 	bucket, ok := obj.(*v1alpha1.BucketStateStore)
 	if !ok {
@@ -68,7 +60,6 @@ func (v *BucketStateStoreCustomValidator) ValidateCreate(ctx context.Context, ob
 	return nil, nil
 }
 
-// ValidateUpdate implements webhook.CustomValidator so a webhook will be registered for the type BucketStateStore.
 func (v *BucketStateStoreCustomValidator) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
 	bucket, ok := newObj.(*v1alpha1.BucketStateStore)
 	if !ok {
@@ -83,7 +74,6 @@ func (v *BucketStateStoreCustomValidator) ValidateUpdate(ctx context.Context, ol
 	return nil, nil
 }
 
-// ValidateDelete implements webhook.CustomValidator so a webhook will be registered for the type BucketStateStore.
 func (v *BucketStateStoreCustomValidator) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
 	return nil, nil
 }
