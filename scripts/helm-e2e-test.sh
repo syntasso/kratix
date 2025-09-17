069ef101b821524c8a48b847b61c676792e0208e
Alparslan Avci <alparslan@syntasso.io>
2025-09-17 16:19:53 +0100

    chore: remove deprecated destination chart from e2e test script (#594)

--- scripts/helm-e2e-test.sh
+++ scripts/helm-e2e-test.sh
@@ -5,19 +5,15 @@ set -eux
 kind delete clusters --all
 
 if [ "$STATE_STORE" == "git" ]; then
-    destination_helm_values_path="hack/destination/helm-values-gitea.yaml"
     platform_helm_values_path="hack/platform/helm-values-gitea.yaml"
     state_store_install_path="hack/platform/gitea-install.yaml"
     job_pod_namespace="gitea"
     job_pod_labels="app.kubernetes.io/instance=gitea"
-    kustomization_namespace="default"
 elif [ "$STATE_STORE" == "bucket" ]; then
-    destination_helm_values_path="hack/destination/helm-values-minio.yaml"
     platform_helm_values_path="hack/platform/helm-values-minio.yaml"
     state_store_install_path="hack/platform/minio-install.yaml"
     job_pod_namespace="kratix-platform-system"
     job_pod_labels="run=minio"
-    kustomization_namespace="kratix-worker-config"
 else
     echo "No supported State Store specified"
     exit 1
@@ -29,7 +25,7 @@ make gitea-cli
 export DOCKER_BUILDKIT=1
 
 echo "setup platform and install StateStore"
-kind create cluster --image kindest/node:v1.27.3 --name platform --config hack/platform/kind-platform-config.yaml
+kind create cluster --image kindest/node:v1.33.1 --name platform --config hack/platform/kind-platform-config.yaml
 make install-cert-manager
 make build-and-load-kratix
 make build-and-load-work-creator
@@ -45,9 +41,15 @@ kubectl --context kind-platform apply --filename "$state_store_install_path"
 kubectl --context kind-platform wait --for=condition=Ready --timeout=300s -n $job_pod_namespace pod -l $job_pod_labels
 
 echo "create worker cluster"
-kind create cluster --image kindest/node:v1.27.3 --name worker --config hack/destination/kind-worker-config.yaml
+kind create cluster --image kindest/node:v1.33.1 --name worker --config hack/destination/kind-worker-config.yaml
 echo "helm install kratix-destination"
-helm install kratix-destination charts/kratix-destination/ -f "$destination_helm_values_path"
+
+extra_args="--path worker-1"
+if [ "$STATE_STORE" == "git" ]; then
+    extra_args="--path .\/destinations\/dev --git"
+fi
+
+./scripts/install-gitops --context kind-worker --platform-cluster-name platform $extra_args
 
 if [ "$STATE_STORE" == "git" ]; then
     copy_gitea_credentials "kind-platform" "kind-worker"
@@ -55,7 +57,7 @@ else
     kubectl --context kind-platform apply --filename config/samples/platform_v1alpha1_worker.yaml
 fi
 
-kubectl --context kind-worker wait --for=condition=Ready --timeout=300s -n "$kustomization_namespace" kustomization kratix-workload-resources
+kubectl --context kind-worker wait --for=condition=Ready --timeout=300s -n flux-system kustomization kratix-worker-resources
 
 echo "helm e2e test setup completed; now verify Jenkins"
 ./scripts/install-jenkins.sh
