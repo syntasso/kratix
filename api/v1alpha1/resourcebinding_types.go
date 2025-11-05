59564e97a5f56e829436ff2de2cb1193818e84db
John Mikos <john@syntasso.io>
2025-11-05 13:02:51 +0000

    wip: changed scaffolded files
    Co-authored-by: Chunyi Lyu <chunyineko@gmail.com>

--- api/v1alpha1/resourcebinding_types.go
+++ api/v1alpha1/resourcebinding_types.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 Syntasso.
+Copyright 2025 Syntasso.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,39 +18,21 @@ package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
-
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // ResourceBindingSpec defines the desired state of ResourceBinding
 type ResourceBindingSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	// The following markers will use OpenAPI v3 schema to validate the value
-	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
-
-	// foo is an example field of ResourceBinding. Edit resourcebinding_types.go to remove/update
-	// +optional
-	Foo *string `json:"foo,omitempty"`
+	// PromiseRevision is the reference to the latest PromiseRevision of a given Promise.
+	// +required
+	PromiseRevision PromiseRevision `json:"promiseRevision"`
+	// ResourceRequest is the reference to the ResourceRequest for a given Promise.
+	// +required
+	ResourceRequest *unstructured.Unstructured `json:"resourceRequest"`
 }
 
 // ResourceBindingStatus defines the observed state of ResourceBinding.
 type ResourceBindingStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// For Kubernetes API conventions, see:
-	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
-
-	// conditions represent the current state of the ResourceBinding resource.
-	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
-	//
-	// Standard condition types include:
-	// - "Available": the resource is fully functional
-	// - "Progressing": the resource is being created or updated
-	// - "Degraded": the resource failed to reach or maintain its desired state
-	//
 	// The status of each condition is one of True, False, or Unknown.
 	// +listType=map
 	// +listMapKey=type
