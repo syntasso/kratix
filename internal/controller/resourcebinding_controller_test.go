7907b0722976b3c85135465896f855072b2e9d61
John Mikos <john@syntasso.io>
2025-11-05 16:04:50 +0000

    wip: changed structure for ResourceBinding, fixed compilation errors
    Co-authored-by: Chunyi Lyu <chunyineko@gmail.com>

--- internal/controller/resourcebinding_controller_test.go
+++ internal/controller/resourcebinding_controller_test.go
@@ -17,68 +17,9 @@ limitations under the License.
 package controller
 
 import (
-	"context"
-
 	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	platformv1alpha1 "github.com/syntasso/kratix/api/v1alpha1"
 )
 
 var _ = Describe("ResourceBinding Controller", func() {
-	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
 
-		ctx := context.Background()
-
-		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
-		}
-		resourcebinding := &platformv1alpha1.ResourceBinding{}
-
-		BeforeEach(func() {
-			By("creating the custom resource for the Kind ResourceBinding")
-			err := k8sClient.Get(ctx, typeNamespacedName, resourcebinding)
-			if err != nil && errors.IsNotFound(err) {
-				resource := &platformv1alpha1.ResourceBinding{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
-						Namespace: "default",
-					},
-					// TODO(user): Specify other spec details if needed.
-				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
-			}
-		})
-
-		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
-			resource := &platformv1alpha1.ResourceBinding{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
-			Expect(err).NotTo(HaveOccurred())
-
-			By("Cleanup the specific resource instance ResourceBinding")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
-		})
-		It("should successfully reconcile the resource", func() {
-			By("Reconciling the created resource")
-			controllerReconciler := &ResourceBindingReconciler{
-				Client: k8sClient,
-				Scheme: k8sClient.Scheme(),
-			}
-
-			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
-				NamespacedName: typeNamespacedName,
-			})
-			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
-		})
-	})
 })
