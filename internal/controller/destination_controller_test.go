bf816a1ae62b660a646ee42273e1ea5e38a5643b
John Mikos <john@syntasso.io>
2026-01-19 11:56:08 +0000

    WIP: restored upstream test against current libraries
    Co-authored-by: Luigi Riefolo <luigi@syntasso.io>

--- internal/controller/destination_controller_test.go
+++ internal/controller/destination_controller_test.go
@@ -499,7 +499,7 @@ func assertCanaryFilesWereCreated(fakeWriter *writersfakes.FakeStateStoreWriter)
 	ExpectWithOffset(1, workPlacementName).To(Equal("kratix-canary"))
 	ExpectWithOffset(1, workloadsToCreate).To(ConsistOf(v1alpha1.Workload{
 		Filepath: "kratix-canary-namespace.yaml",
-		Content:  "apiVersion: v1\nkind: Namespace\nmetadata:\n  name: kratix-worker-system\nspec: {}\nstatus: {}\n",
+		Content:  "apiVersion: v1\nkind: Namespace\nmetadata:\n  creationTimestamp: null\n  name: kratix-worker-system\nspec: {}\nstatus: {}\n",
 	}))
 	ExpectWithOffset(1, workloadsToDelete).To(BeNil())
 
@@ -508,7 +508,7 @@ func assertCanaryFilesWereCreated(fakeWriter *writersfakes.FakeStateStoreWriter)
 	ExpectWithOffset(1, workPlacementName).To(Equal("kratix-canary"))
 	ExpectWithOffset(1, workloadsToCreate).To(ConsistOf(v1alpha1.Workload{
 		Filepath: "kratix-canary-configmap.yaml",
-		Content:  "apiVersion: v1\ndata:\n  canary: this confirms your infrastructure is reading from Kratix state stores\nkind: ConfigMap\nmetadata:\n  name: kratix-info\n  namespace: kratix-worker-system\n",
+		Content:  "apiVersion: v1\ndata:\n  canary: this confirms your infrastructure is reading from Kratix state stores\nkind: ConfigMap\nmetadata:\n  creationTimestamp: null\n  name: kratix-info\n  namespace: kratix-worker-system\n",
 	}))
 	ExpectWithOffset(1, workloadsToDelete).To(BeNil())
 }
