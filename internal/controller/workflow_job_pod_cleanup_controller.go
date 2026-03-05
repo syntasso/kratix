d17908439b3746969910b2ddb4c95f6636cff737
Jake Klein <jake@syntasso.io>
2026-03-05 10:02:32 +0000

    feat: Add option to cleanup workflow job pods after completion (#719)
    * use labelselector predicate
    * add rbac
    * chore: check ownership of pod
    * fix: make sure pods can be cleaned up after controller restart
    - when controller is restarted and there are jobs at terminal state
    before the controller is started, there are create event but not update
    event
    
    ---------
    
    Co-authored-by: Chunyi Lyu <chunyineko@gmail.com>

---
+++ internal/controller/workflow_job_pod_cleanup_controller.go
@@ -0,0 +1,202 @@
+/*
+Copyright 2021 Syntasso.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-logr/logr"
+	"github.com/syntasso/kratix/api/v1alpha1"
+	"github.com/syntasso/kratix/internal/logging"
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+)
+
+// WorkflowJobPodCleanupReconciler removes terminal Pods from terminal Kratix workflow Jobs after a configured TTL.
+type WorkflowJobPodCleanupReconciler struct {
+	Client              client.Client
+	Log                 logr.Logger
+	PodTTLAfterFinished time.Duration
+}
+
+// +kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch
+// +kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch;delete
+
+func (r *WorkflowJobPodCleanupReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, retErr error) {
+	logger := r.Log.WithValues(
+		"controller", "workflow-job-pod-cleanup",
+		"name", req.Name,
+		"namespace", req.Namespace,
+	)
+
+	job := &batchv1.Job{}
+	if err := r.Client.Get(ctx, req.NamespacedName, job); err != nil {
+		if kerrors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	if !shouldReconcileWorkflowJob(job) {
+		return ctrl.Result{}, nil
+	}
+
+	terminalTime, ok := getJobTerminalTime(job)
+	if !ok {
+		logging.Debug(logger, "job is terminal but no terminal timestamp is available; skipping pod cleanup")
+		return ctrl.Result{}, nil
+	}
+
+	expirationTime := terminalTime.Add(r.PodTTLAfterFinished)
+	now := time.Now()
+	if now.Before(expirationTime) {
+		return ctrl.Result{RequeueAfter: expirationTime.Sub(now)}, nil
+	}
+
+	podList := &corev1.PodList{}
+	if err := r.Client.List(
+		ctx,
+		podList,
+		client.InNamespace(job.GetNamespace()),
+		client.MatchingLabels{batchv1.JobNameLabel: job.GetName()},
+	); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	for i := range podList.Items {
+		pod := &podList.Items[i]
+		if isPodOwnedByJob(pod, job) {
+			err := r.Client.Delete(ctx, pod)
+			if err != nil {
+				if kerrors.IsNotFound(err) {
+					continue
+				}
+				return ctrl.Result{}, err
+			}
+			logging.Info(logger, "deleted terminal workflow pod", "pod", pod.GetName(), "job", job.GetName())
+		}
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func (r *WorkflowJobPodCleanupReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	managedByKratixPredicate, err := predicate.LabelSelectorPredicate(metav1.LabelSelector{
+		MatchLabels: map[string]string{
+			v1alpha1.ManagedByLabel: v1alpha1.ManagedByLabelValue,
+		},
+	})
+	if err != nil {
+		return fmt.Errorf("failed to build managed-by label selector predicate: %w", err)
+	}
+
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&batchv1.Job{}, builder.WithPredicates(
+			terminalWorkflowJobPredicate(),
+			managedByKratixPredicate,
+		)).
+		Complete(r)
+}
+
+func terminalWorkflowJobPredicate() predicate.Funcs {
+	return predicate.Funcs{
+		CreateFunc: func(e event.CreateEvent) bool {
+			job, ok := e.Object.(*batchv1.Job)
+			return ok && shouldReconcileWorkflowJob(job)
+		},
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			job, ok := e.ObjectNew.(*batchv1.Job)
+			return ok && shouldReconcileWorkflowJob(job)
+		},
+		DeleteFunc: func(event.DeleteEvent) bool {
+			return false
+		},
+		GenericFunc: func(event.GenericEvent) bool {
+			return false
+		},
+	}
+}
+
+func shouldReconcileWorkflowJob(job *batchv1.Job) bool {
+	if job == nil {
+		return false
+	}
+	return isJobTerminal(job)
+}
+
+func isJobTerminal(job *batchv1.Job) bool {
+	for _, condition := range job.Status.Conditions {
+		if condition.Status != corev1.ConditionTrue {
+			continue
+		}
+
+		if condition.Type == batchv1.JobComplete || condition.Type == batchv1.JobFailed {
+			return true
+		}
+	}
+	return false
+}
+
+func getJobTerminalTime(job *batchv1.Job) (time.Time, bool) {
+	if job.Status.CompletionTime != nil {
+		return job.Status.CompletionTime.Time, true
+	}
+
+	var latestFailedConditionTime metav1.Time
+	hasFailedConditionTime := false
+	for _, condition := range job.Status.Conditions {
+		// Successful Jobs should always have completionTime set.
+		// If completionTime is absent, only failed Jobs can provide a terminal time via status conditions.
+		if condition.Type != batchv1.JobFailed || condition.Status != corev1.ConditionTrue {
+			continue
+		}
+		if condition.LastTransitionTime.IsZero() {
+			continue
+		}
+
+		if !hasFailedConditionTime || condition.LastTransitionTime.After(latestFailedConditionTime.Time) {
+			latestFailedConditionTime = condition.LastTransitionTime
+			hasFailedConditionTime = true
+		}
+	}
+
+	if !hasFailedConditionTime {
+		return time.Time{}, false
+	}
+	return latestFailedConditionTime.Time, true
+}
+
+func isPodOwnedByJob(pod *corev1.Pod, job *batchv1.Job) bool {
+	for _, ownerReference := range pod.GetOwnerReferences() {
+		if ownerReference.Kind != "Job" || ownerReference.Name != job.GetName() {
+			continue
+		}
+		if len(ownerReference.UID) == 0 || len(job.GetUID()) == 0 || ownerReference.UID == job.GetUID() {
+			return true
+		}
+	}
+	return false
+}
---
+++ internal/controller/workflow_job_pod_cleanup_controller_test.go
@@ -0,0 +1,214 @@
+/*
+Copyright 2021 Syntasso.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller_test
+
+import (
+	"context"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/syntasso/kratix/api/v1alpha1"
+	"github.com/syntasso/kratix/internal/controller"
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var _ = Describe("WorkflowJobPodCleanupReconciler", func() {
+	var (
+		ctx        = context.Background()
+		reconciler *controller.WorkflowJobPodCleanupReconciler
+	)
+
+	BeforeEach(func() {
+		reconciler = &controller.WorkflowJobPodCleanupReconciler{
+			Client:              fakeK8sClient,
+			Log:                 ctrl.Log.WithName("controllers").WithName("WorkflowJobPodCleanup"),
+			PodTTLAfterFinished: time.Minute,
+		}
+	})
+
+	When("the job has been terminal for at least the configured ttl", func() {
+		It("deletes pods owned by the job", func() {
+			completedAt := time.Now().Add(-2 * time.Minute)
+			job := terminalJob("example-job", "default", "example-job-uid", completedAt)
+			Expect(fakeK8sClient.Create(ctx, job)).To(Succeed())
+
+			succeededPod := jobOwnedPod("example-pod-succeeded", job)
+			failedPod := jobOwnedPod("example-pod-failed", job)
+
+			anotherJob := terminalJob("another-job", "default", "another-job-uid", completedAt)
+			Expect(fakeK8sClient.Create(ctx, anotherJob)).To(Succeed())
+			anotherJobPod := jobOwnedPod("another-job-pod", anotherJob)
+
+			Expect(fakeK8sClient.Create(ctx, succeededPod)).To(Succeed())
+			Expect(fakeK8sClient.Create(ctx, failedPod)).To(Succeed())
+			Expect(fakeK8sClient.Create(ctx, anotherJobPod)).To(Succeed())
+
+			result, err := reconciler.Reconcile(ctx, ctrl.Request{NamespacedName: client.ObjectKeyFromObject(job)})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(result).To(Equal(ctrl.Result{}))
+
+			Expect(isNotFound(ctx, client.ObjectKeyFromObject(succeededPod), &corev1.Pod{})).To(BeTrue())
+			Expect(isNotFound(ctx, client.ObjectKeyFromObject(failedPod), &corev1.Pod{})).To(BeTrue())
+			Expect(fakeK8sClient.Get(ctx, client.ObjectKeyFromObject(anotherJobPod), &corev1.Pod{})).To(Succeed())
+		})
+	})
+
+	When("the job ttl has not elapsed", func() {
+		It("requeues after the remaining ttl and does not delete pods", func() {
+			job := terminalJob("example-job", "default", "example-job-uid", time.Now().Add(-30*time.Second))
+			Expect(fakeK8sClient.Create(ctx, job)).To(Succeed())
+
+			succeededPod := jobOwnedPod("example-pod-succeeded", job)
+			Expect(fakeK8sClient.Create(ctx, succeededPod)).To(Succeed())
+
+			result, err := reconciler.Reconcile(ctx, ctrl.Request{NamespacedName: client.ObjectKeyFromObject(job)})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(result.RequeueAfter).To(BeNumerically(">", 25*time.Second))
+			Expect(result.RequeueAfter).To(BeNumerically("<=", 30*time.Second))
+			Expect(fakeK8sClient.Get(ctx, client.ObjectKeyFromObject(succeededPod), &corev1.Pod{})).To(Succeed())
+		})
+	})
+
+	When("the job is not terminal", func() {
+		It("does not requeue and does not delete pods", func() {
+			job := nonTerminalJob("example-job", "default", "example-job-uid")
+			Expect(fakeK8sClient.Create(ctx, job)).To(Succeed())
+
+			succeededPod := jobOwnedPod("example-pod-succeeded", job)
+			Expect(fakeK8sClient.Create(ctx, succeededPod)).To(Succeed())
+
+			result, err := reconciler.Reconcile(ctx, ctrl.Request{NamespacedName: client.ObjectKeyFromObject(job)})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(result).To(Equal(ctrl.Result{}))
+			Expect(fakeK8sClient.Get(ctx, client.ObjectKeyFromObject(succeededPod), &corev1.Pod{})).To(Succeed())
+		})
+	})
+
+	When("the job has failed and therefore is no status.completionTime set on the job", func() {
+		It("uses failed condition transition time to evaluate ttl", func() {
+			job := failedJobWithoutCompletionTime("example-job", "default", "example-job-uid", time.Now().Add(-2*time.Minute))
+			Expect(fakeK8sClient.Create(ctx, job)).To(Succeed())
+
+			succeededPod := jobOwnedPod("example-pod-succeeded", job)
+			Expect(fakeK8sClient.Create(ctx, succeededPod)).To(Succeed())
+
+			result, err := reconciler.Reconcile(ctx, ctrl.Request{NamespacedName: client.ObjectKeyFromObject(job)})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(result).To(Equal(ctrl.Result{}))
+			Expect(isNotFound(ctx, client.ObjectKeyFromObject(succeededPod), &corev1.Pod{})).To(BeTrue())
+		})
+	})
+})
+
+func terminalJob(name, namespace string, uid types.UID, completedAt time.Time) *batchv1.Job {
+	return &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			UID:       uid,
+			Labels: map[string]string{
+				v1alpha1.ManagedByLabel: v1alpha1.ManagedByLabelValue,
+			},
+		},
+		Status: batchv1.JobStatus{
+			CompletionTime: &metav1.Time{Time: completedAt},
+			Conditions: []batchv1.JobCondition{
+				{
+					Type:               batchv1.JobComplete,
+					Status:             corev1.ConditionTrue,
+					LastTransitionTime: metav1.Time{Time: completedAt},
+				},
+			},
+		},
+	}
+}
+
+func failedJobWithoutCompletionTime(name, namespace string, uid types.UID, terminalAt time.Time) *batchv1.Job {
+	return &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			UID:       uid,
+			Labels: map[string]string{
+				v1alpha1.ManagedByLabel: v1alpha1.ManagedByLabelValue,
+			},
+		},
+		Status: batchv1.JobStatus{
+			Conditions: []batchv1.JobCondition{
+				{
+					Type:               batchv1.JobFailed,
+					Status:             corev1.ConditionTrue,
+					LastTransitionTime: metav1.Time{Time: terminalAt},
+				},
+			},
+		},
+	}
+}
+
+func nonTerminalJob(name, namespace string, uid types.UID) *batchv1.Job {
+	return &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			UID:       uid,
+			Labels: map[string]string{
+				v1alpha1.ManagedByLabel: v1alpha1.ManagedByLabelValue,
+			},
+		},
+		Status: batchv1.JobStatus{
+			Conditions: []batchv1.JobCondition{
+				{
+					Type:   batchv1.JobComplete,
+					Status: corev1.ConditionFalse,
+				},
+			},
+		},
+	}
+}
+
+func jobOwnedPod(name string, job *batchv1.Job) *corev1.Pod {
+	controllerValue := true
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: job.GetNamespace(),
+			Labels: map[string]string{
+				batchv1.JobNameLabel: job.GetName(),
+			},
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					APIVersion: batchv1.SchemeGroupVersion.String(),
+					Kind:       "Job",
+					Name:       job.GetName(),
+					UID:        job.GetUID(),
+					Controller: &controllerValue,
+				},
+			},
+		},
+	}
+}
+
+func isNotFound(ctx context.Context, key client.ObjectKey, obj client.Object) bool {
+	return kerrors.IsNotFound(fakeK8sClient.Get(ctx, key, obj))
+}
