9970ba204a6ae1161c8bcb41098c0109112cd42f
Chunyi Lyu <chunyineko@gmail.com>
2026-03-24 16:31:29 +0000

    chore: helpers to parse retryAfter in work-creator
    Co-authored-by: Sapphire Mason-Brown <sapphire@syntasso.io>

---
+++ work-creator/lib/workflow_control_test.go
@@ -0,0 +1,20 @@
+package lib_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+)
+
+var _ = Describe("WorkflowControl", func() {
+	When("the file is valid", func() {
+		It("can parse it", func() {
+
+		})
+	})
+
+	When("the file has an invalid 'retryAfter' configuration", func() {
+		It("returns a specific error", func() {
+
+		})
+	})
+
+})
